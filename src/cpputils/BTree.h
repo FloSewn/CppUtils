40ec245a6bdcb261ab37f74132b50f84667b3418
FloSewn <florian.setzwein@gmx.de>
2022-09-25 15:51:43 +0200

    Update BTree

--- src/cpputils/BTree.h
+++ src/cpputils/BTree.h
@@ -45,13 +45,9 @@ class BTree;
 *    C... holding values >= 38 && < 57
 *    D... holding values >= 57 
 *
-*   
-*********************************************************************/
-
-/*********************************************************************
 * The BTree node implementation
 * -> T... key-type
-* -> M... order of the B-Tree
+* -> M... minimum degree of the B-Tree
 *********************************************************************/
 template <typename T, long unsigned int M>
 class BTreeNode
@@ -96,15 +92,8 @@ public:
   Keys& keys() { return keys_; }
   const Keys& keys() const { return keys_; }
 
-  T* key(std::size_t i) { return keys_[i]; }
-  const T* key(std::size_t i) const { return keys_[i]; }
-
   Children& children() { return children_; }
   const Children& children() const { return children_; }
-
-  BTreeNode<T,M>* child(std::size_t i) { return children_[i].get(); }
-  const BTreeNode<T,M>* child(std::size_t i) const 
-  { return children_[i].get(); }
 
   BTreeNode<T,M>* parent() { return parent_; }
   const BTreeNode<T,M>* parent() const { return parent_; }
@@ -114,7 +103,70 @@ public:
   long unsigned int n_keys() const { return n_keys_; }
 
 
+  /*------------------------------------------------------------------ 
+  | Access keys
+  ------------------------------------------------------------------*/
+  T* key(std::size_t i) 
+  { 
+    ASSERT( i < n_keys_, "Invalid BTree access.");
+    return keys_[i]; 
+  }
+  const T* key(std::size_t i) const 
+  { 
+    ASSERT( i < n_keys_, "Invalid BTree access.");
+    return keys_[i]; 
+  }
+
+  /*------------------------------------------------------------------ 
+  | Access children
+  ------------------------------------------------------------------*/
+  std::unique_ptr<BTreeNode<T,M>>& child_ptr(std::size_t i) 
+  { 
+    ASSERT( i <= n_keys_, "Invalid BTree access.");
+    return children_[i]; 
+  }
+
+  const std::unique_ptr<BTreeNode<T,M>>& child_ptr(std::size_t i) const
+  { 
+    ASSERT( i <= n_keys_, "Invalid BTree access.");
+    return children_[i]; 
+  }
+
+  BTreeNode<T,M>* child(std::size_t i) 
+  { 
+    ASSERT( i <= n_keys_, "Invalid BTree access.");
+    return children_[i].get(); 
+  }
+  const BTreeNode<T,M>* child(std::size_t i) const 
+  { 
+    ASSERT( i <= n_keys_, "Invalid BTree access.");
+    return children_[i].get(); 
+  }
+
+
 private:
+  /*------------------------------------------------------------------ 
+  | Set keys
+  ------------------------------------------------------------------*/
+  void key(std::size_t i, T* k) 
+  { 
+    if ( i >= n_keys_ )
+      throw std::runtime_error("Invalid BTree access.");
+    keys_[i] = k;
+  }
+
+  /*------------------------------------------------------------------ 
+  | Set children
+  ------------------------------------------------------------------*/
+  void child(std::size_t i, std::unique_ptr<BTreeNode<T,M>>& c) 
+  { 
+    if ( i > n_keys_ )
+      throw std::runtime_error("Invalid BTree access.");
+    children_[i] = std::move(c);
+  }
+
+
+
   /*------------------------------------------------------------------ 
   | Attributes
   ------------------------------------------------------------------*/
@@ -153,25 +205,32 @@ public:
   const BTreeNode<T,M>& root() const { return *root_; }
 
   /*------------------------------------------------------------------ 
-  | Search
+  | Search for a key in the tree
   ------------------------------------------------------------------*/
-  const BTreeNode<T,M>* search(const T& key, const BTreeNode<T,M>& node)
-  const
+  const BTreeNode<T,M>* search(const T& key, 
+                               const BTreeNode<T,M>& node) const
   {
     const BTreeNode<T,M>* cur = &node;
 
-    std::size_t i = 1;
+    if ( node.n_keys() == 0 ) 
+      return nullptr;
 
-    while ( i < node.n_keys()  &&  key > *node.key(i-1) )
+    std::size_t i = 0;
+
+    while ( i < node.n_keys()  &&  key > *node.key(i) )
       ++i;
 
-    if ( i <= node.n_keys()  &&  key == *node.key(i-1) )
+    if ( i < node.n_keys()  &&  key == *node.key(i) )
+    {
       return cur;
+    }
     else if ( node.is_leaf() )
+    {
       return nullptr;
+    }
     else
     {
-      const BTreeNode<T,M>* child = node.child(i-1);
+      const BTreeNode<T,M>* child = node.child(i);
 
       if ( !child )
         throw std::runtime_error(
@@ -192,15 +251,25 @@ public:
     // --> The root splits and a new node becomes the root 
     if ( root_->n_keys() == 2*M-1 )
     {
+      // This node will be the new root
       auto new_node = std::make_unique<BTreeNode<T,M>>();
 
+      // new_node gets its first key in split_child()
       new_node->is_leaf_ = false;
       new_node->n_keys_  = 0;
 
-      new_node->children_[0] = std::move(root_);
-      root_                  = std::move(new_node);
+      new_node->child(0, root_);
+      root_ = std::move(new_node);
 
+      // The old root node will now be divided in two nodes with 
+      // (M-1) keys each and move the remaining median key into 
+      // the new root node
       split_child(*root_, 0);
+
+      ASSERT(root_->n_keys_ == 1, 
+             "Invalid number of keys in root node." );
+
+      // Next, insert the key 
       insert_nonfull(*root_, key);
     }
     else
@@ -209,44 +278,125 @@ public:
   } // insert() */
 
   /*------------------------------------------------------------------ 
-  | Insert nonfull
+  | Insert a key into a nonfull node
+  |
+  | Arguments:
+  | ----------
+  | node.... nonfull node (branch or leaf)
+  | key..... the key that will be inserted to the tree
+  |
+  | 
+  |  Example: M = 4,  node is leaf,  input key = 3
+  |  -------
+  |  
+  |  :                       :
+  |  |                       |
+  |  .---------------.       .---------------.
+  |  | 1 4 5 7 . . . |  -->  | 1 3 4 5 7 . . |
+  |  '---------------'       '---------------'
+  |  
+  |  
+  |  Example: M = 4,  node is branch,  input key = 15
+  |  -------
+  |                            1) Pick child C (>=10 & <23)
+  |  :                  
+  |  |                  
+  |  .----------------------.  
+  |  |  3 10 23 43 60  .  . |  
+  |  '----------------------'  
+  |   |  |  |  |  |
+  |   |  |  |  |  |
+  |   A  B  |  D  E
+  |         | 
+  |         .----------------------.
+  |       C | 11 13 14 17 19 20 22 |
+  |         '----------------------'
+  |
+  |  Step 1) Split C & add new node N
+  |  --------------------------------
+  |  :                  
+  |  |                  
+  |  .----------------------.  
+  |  |  3 10 17 23 43 60  . |  
+  |  '----------------------'  
+  |   |  |  |  |  |  |
+  |   |  |  |  |  |  |
+  |   A  B  |  |  D  E
+  |         |  |
+  |         .----------------------.
+  |       C | 11 13 14  .  .  .  . |
+  |         '----------------------'
+  |            |
+  |            |
+  |            .----------------------.
+  |          N | 19 20 22  .  .  .  . |
+  |            '----------------------'
+  |
+  |  Step 2) Add input key 15 to node C
+  |  ----------------------------------
+  |  :                  
+  |  |                  
+  |  .----------------------.  
+  |  |  3 10 17 23 43 60  . |  
+  |  '----------------------'  
+  |   |  |  |  |  |  |
+  |   |  |  |  |  |  |
+  |   A  B  |  |  D  E
+  |         |  |
+  |         .----------------------.
+  |       C | 11 13 14 15  .  .  . |
+  |         '----------------------'
+  |            |
+  |            |
+  |            .----------------------.
+  |          N | 19 20 22  .  .  .  . |
+  |            '----------------------'
+  |                    
   ------------------------------------------------------------------*/
   void insert_nonfull(BTreeNode<T,M>& node, T* key)
   {
     auto i = node.n_keys();
 
-    // A leaf node is given -> insert key 
+    // Check that the given node is nonfull
+    ASSERT(node.n_keys() != (2*M-1), "Invalid BTree structure");
+
+    // ---- Leaf node ----
     if ( node.is_leaf() )
     {
-      // Shift larger keys to the right
-      while ( i > 0 && (*key < *node.keys_[i-1]) )
+      ++node.n_keys_;
+
+      // Traverse all node keys until input key is greatest
+      while ( i > 0 && (*key < *node.key(i-1)) )
       {
-        node.keys_[i] = node.keys_[i-1];
+        node.key(i, node.key(i-1));
         --i;
       }
 
       // Place key
-      node.keys_[i] = key;
-      ++node.n_keys_;
+      node.key(i, key);
     }
+    // ---- Branch node ----
     else 
     {
-      // Got to the right key
-      while ( i > 0 && (*key < *node.keys_[i-1]) )
+      // Traverse all node keys until input key is greatest
+      while ( i > 0 && (*key < *node.key(i-1)) )
         --i;
 
-      // Point to child
-      ++i;
-
-      if ( node.children_[i-1]->n_keys() == 2*M-1 )
+      // In case that the found child is full, split it in two nodes
+      if ( node.child(i)->n_keys() == 2*M-1 )
       {
-        split_child( node, i-1 );
+        // Split the node's i-th child in two nodes
+        // -> This inserts a new key to the current node 
+        //    at position i
+        split_child( node, i );
 
-        if ( *key > *node.keys_[i-1] )
+        // If input key is larger than new key of current node,
+        // insert input key in newly generated node
+        if ( *key > *node.key(i) )
           ++i;
       }
 
-      insert_nonfull(*node.children_[i-1], key);
+      insert_nonfull(*node.child(i), key);
     }
 
   } // insert_nonfull() 
@@ -255,7 +405,7 @@ public:
   /*------------------------------------------------------------------ 
   | Split a full node (with 2*M-1 keys) around its median key 
   | into two nodes with (M-1) keys each.
-  | The median key moves up into the node's parent. 
+  | The median key moves up into the nonfull node's parent. 
   |
   | Arguments:
   | ----------
@@ -286,13 +436,16 @@ public:
   ------------------------------------------------------------------*/
   void split_child(BTreeNode<T,M>& parent_node, std::size_t i)
   {
+    // Check that the parent node is nonfull
+    ASSERT(parent_node.n_keys() != (2*M-1), "Invalid BTree structure");
+
     // This is the new node. It gets the keys of the full node,
     // that are greated than the median
     auto new_node = std::make_unique<BTreeNode<T,M>>();
 
     // Child must be a full node
-    BTreeNode<T,M>* child_node = parent_node.children_[i].get();
-    ASSERT( child_node->n_keys() == (2*M-1), "Invalid BTree structure");
+    BTreeNode<T,M>* child_node = parent_node.child(i);
+    ASSERT(child_node->n_keys() == (2*M-1), "Invalid BTree structure");
 
     new_node->is_leaf_ = child_node->is_leaf();
     new_node->n_keys_  = M-1;
@@ -300,23 +453,24 @@ public:
     // Put all keys of the full child into the new node,
     // which are greater than the median (=M)
     for (std::size_t j = 0; j < M-1; ++j)
-      new_node->keys_[j] = child_node->keys_[j+M];
+      new_node->key(j, child_node->key(j+M));
 
     // If the child node is not a leaf, pass also its 
     // children to the new node
     if ( !child_node->is_leaf() )
       for (std::size_t j = 0; j < M; ++j) 
-        new_node->children_[j] = std::move(child_node->children_[j+M]);
+        new_node->child(j, child_node->child_ptr(j+M));
 
     // Update number of keys 
     child_node->n_keys_ = M-1;
 
-    // new_node will be placed at parent_node.child(i+1),
+    // new_node will be placed at parent node's (i+1)-th child,
     // so all other children at j > i must be shifted to the right
-    for (std::size_t j = parent_node.n_keys()+1; j > i; --j)
-      parent_node.children_[j] = std::move(parent_node.children_[j-1]);
+    for (std::size_t j = parent_node.n_keys()+1; j > i+1; --j)
+      parent_node.child(j, parent_node.child_ptr(j-1));
 
-    parent_node.children_[i] = std::move( new_node );
+    ++parent_node.n_keys_;
+    parent_node.child(i+1, new_node);
 
     // All following keys in parent_node must be shifted to the right
     for (std::size_t j = parent_node.n_keys()-1; j > i-1; --j)
--- src/test/tests_BTree.cpp
+++ src/test/tests_BTree.cpp
@@ -115,6 +115,9 @@ void insert_nonfull_first_equal()
 --------------------------------------------------------------------*/
 void insert_nonfull_second()
 {
+  DEBUG_LOG("\n-------------------------------------------");
+  DEBUG_LOG(" Test function: insert_nonfull_second() ");
+
   BTree<int,2> btree;
   std::vector<int> data {};
 
@@ -122,6 +125,15 @@ void insert_nonfull_second()
   data.push_back(1);
   data.push_back(2);
   data.push_back(3);
+  
+  data.push_back(4);
+  data.push_back(5);
+
+  data.push_back(6);
+  data.push_back(7);
+  data.push_back(8);
+  data.push_back(9);
+  data.push_back(10);
 
   const BTreeNode<int,2>* found = nullptr;
 
@@ -137,10 +149,61 @@ void insert_nonfull_second()
   found = btree.search(data[2], btree.root());
   CHECK( found->key(2) == &data[2] );
 
+  DEBUG_LOG("ROOT");
+  for (std::size_t i = 0; i < btree.root().n_keys(); ++i)
+    DEBUG_LOG(*btree.root().key(i));
+
+
   // BTree root should split now
   btree.insert( &data[3] );
+
+  CHECK( !btree.root().is_leaf() );
+  CHECK( btree.root().n_keys() == 1);
+  CHECK( btree.root().child(0) != nullptr);
+  CHECK( btree.root().child(1) != nullptr);
+
   found = btree.search(data[3], btree.root());
+  CHECK( found != nullptr );
   CHECK( found->key(1) == &data[3] );
+
+  btree.insert( &data[4] );
+  found = btree.search(data[4], btree.root());
+  CHECK( found != nullptr );
+  CHECK( found->key(2) == &data[4] );
+
+  btree.insert( &data[5] );
+  found = btree.search(data[5], btree.root());
+  CHECK( found != nullptr );
+  CHECK( found->key(1) == &data[5] );
+
+  btree.insert( &data[6] );
+  btree.insert( &data[7] );
+  btree.insert( &data[8] );
+  btree.insert( &data[9] );
+  btree.insert( &data[10] );
+
+  found = btree.search(data[10], btree.root());
+  CHECK( found != nullptr );
+  CHECK( found->key(2) == &data[10] );
+
+
+  /*
+  DEBUG_LOG("ROOT");
+  for (std::size_t i = 0; i < btree.root().n_keys(); ++i)
+    DEBUG_LOG(*btree.root().key(i));
+
+  DEBUG_LOG("CHILD 1");
+  for (std::size_t i = 0; i < btree.root().child(0)->n_keys(); ++i)
+    DEBUG_LOG(*btree.root().child(0)->key(i));
+
+  DEBUG_LOG("CHILD 2");
+  for (std::size_t i = 0; i < btree.root().child(1)->n_keys(); ++i)
+    DEBUG_LOG(*btree.root().child(1)->key(i));
+
+  //DEBUG_LOG("CHILD 3");
+  for (std::size_t i = 0; i < btree.root().child(2)->n_keys(); ++i)
+    DEBUG_LOG(*btree.root().child(2)->key(i));
+  */
 
 
 } // insert_nonfull_second()
